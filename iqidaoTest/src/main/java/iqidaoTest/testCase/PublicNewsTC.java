d344a7fc2592239790747768f9475fe3ae19fe1d
zhangjiya <xsdgctjy@163.com>
2018-01-09 15:32:40 +0800

    mod_case_param
    mod_case_param

--- iqidaoTest/src/main/java/iqidaoTest/testCase/PublicNewsTC.java
+++ iqidaoTest/src/main/java/iqidaoTest/testCase/PublicNewsTC.java
@@ -2,7 +2,9 @@ package iqidaoTest.testCase;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -11,48 +13,42 @@ import iqidaoTest.PublicColumnObject.PublicColumn;
 import iqidaoTest.PublicColumnObject.PublicColumnList;
 import iqidaoTest.adminPageObject.AdminHomePage;
 import iqidaoTest.adminPageObject.AdminLoginPage;
+import iqidaoTest.Utils.CommonUtils;
+import iqidaoTest.Utils.xmlData;
 
 public class PublicNewsTC {
 	private WebDriver driver;
 	// 定义页面url
-	String adminLoginUrl = "http://testing.iqidao.com/admin001";
-	String adminHomeUrl = "http://testing.iqidao.com/admin001/home";
-	String publicurl = "http://testing.iqidao.com/admin001/resource/public/article";
-	/*
-	 * WebElement targetText=this.driver.findElement(By.xpath(
-	 * "/html/body/div[1]/div/section[2]/section/header/h3"));
-	 */ // 登录参数值
-	String userName = "186186";
-	String passWord = "111111";
+	String adminLoginUrl = xmlData.getParamFromXml("adminLoginUrl");
+	String adminHomeUrl = xmlData.getParamFromXml("adminHomeUrl");
+	String publicurl = xmlData.getParamFromXml("publicurl");
+	// 登录参数值
+	String userName = xmlData.getParamFromXml("userName");
+	String passWord = xmlData.getParamFromXml("passWord");
 	// 专栏信息添加参数值
-	String newname = "zjy_news";
-	String modnewname = "zjy_mod";
-	String newsclass = "1";
+	String newname = xmlData.getParamFromXml("newname");
+	String modnewname = xmlData.getParamFromXml("modnewname");
+	String newsclass = xmlData.getParamFromXml("newsclass");
 
 	@BeforeTest
-	public void beforeTest() {
-		// this.driver = new FirefoxDriver();
+	public void BeforeTest() {
 		System.setProperty("webdriver.chrome.driver", "C:\\231\\chromedriver.exe");
 		this.driver = new ChromeDriver();
 		this.driver.manage().window().maximize();
+		AdminLoginPage adminLoginPage = new AdminLoginPage(this.driver, adminLoginUrl);
+		AdminHomePage adminHomePage = adminLoginPage.adminLogin(userName, passWord, adminHomeUrl);
 	}
 
 	@Test
 	public void publicclumnadd() {
-		// 登录
-		AdminLoginPage adminLoginPage = new AdminLoginPage(this.driver, adminLoginUrl);
-		AdminHomePage adminHomePage = adminLoginPage.adminLogin(userName, passWord, adminHomeUrl);
 		PublicColumn add = new PublicColumn(this.driver, publicurl);
 		add.addColoumn(newname, newsclass);
 		PublicColumnList list = new PublicColumnList(this.driver, publicurl);
-		list.judgeif(newname); 	
-		}
+		list.judgeif(newname);
+	}
 
 	@Test(dependsOnMethods = { "publicclumnadd" })
 	public void publicclumnmod() {
-		// 登录
-		AdminLoginPage adminLoginPage = new AdminLoginPage(this.driver, adminLoginUrl);
-		AdminHomePage adminHomePage = adminLoginPage.adminLogin(userName, passWord, adminHomeUrl);
 		// 查询到已添加信息
 		PublicColumnList list = new PublicColumnList(this.driver, publicurl);
 		list.Search(newname);
@@ -68,17 +64,15 @@ public class PublicNewsTC {
 		SwitchTo switch1 = new SwitchTo();
 		switch1.changewindow(this.driver);
 		list.ModColoumn(modnewname);
-		//检查是否修改成功
+		// 检查是否修改成功
 		this.driver.navigate().refresh();
 		list.judgeif(modnewname);
 	}
 
 	// 删除案例
-	@Test (dependsOnMethods = { "publicclumnmod" })
+	@Test(dependsOnMethods = { "publicclumnmod" })
 	public void publicclumndel() throws InterruptedException {
 		// 登录
-		AdminLoginPage adminLoginPage = new AdminLoginPage(this.driver, adminLoginUrl);
-		AdminHomePage adminHomePage = adminLoginPage.adminLogin(userName, passWord, adminHomeUrl);
 		PublicColumnList list = new PublicColumnList(this.driver, publicurl);
 		list.DEL(modnewname);
 		// 判断是否删除
