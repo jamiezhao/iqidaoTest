5aac55ccd40ba86d8f4ba481fc35c6b04e73ff13
zhangjiya <xsdgctjy@163.com>
2018-01-10 16:09:41 +0800

    Revert "Mod name/value/class/url"
    This reverts commit ed49e955907158f284b3fd0123e0ce05eb859c4c.

--- iqidaoTest/src/main/java/iqidaoTest/testCase/CouserPaperQuizTC.java
+++ iqidaoTest/src/main/java/iqidaoTest/testCase/CouserPaperQuizTC.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import iqidaoTest.CoursePaper.CourseQuizAddPage;
+import iqidaoTest.CoursePaper.CourseQuizAdd;
 import iqidaoTest.CoursePaper.PaperListPage;
 import iqidaoTest.CoursePaper.SwitchTo;
 import iqidaoTest.Utils.CommonUtils;
@@ -23,25 +23,24 @@ public class CouserPaperQuizTC {
 	String adminHomeUrl = xmlData.getParamFromXml("adminHomeUrl");
 	String paperSearchUrl = xmlData.getParamFromXml("paperSearchUrl");
 	// 登录
-	String ChormeURL=xmlData.getParamFromXml("ChormeURL");
 	String userName = xmlData.getParamFromXml("userName");
 	String passWord = xmlData.getParamFromXml("passWord");
 	// 查询试卷
-	String CourseName = xmlData.getParamFromXml("itemName");
+	String CourseName = xmlData.getParamFromXml("CourseName");
 	String exameName = xmlData.getParamFromXml("exameName");
-	String papername =CourseName+"练习题";
+	String papername = xmlData.getParamFromXml("papername");
 	// 0-系统试卷；1专项考试；2-专项预习；3-专项课后；4-错题本；5推荐试卷
 	String orginselect = xmlData.getParamFromXml("orginselect");
-	String papername1 =CourseName+"预习题";
+	String papername1 = xmlData.getParamFromXml("papername1");
 	// 0-系统试卷；1专项考试；2-专项预习；3-专项课后；4-错题本；5推荐试卷
 	String orginselect1 = xmlData.getParamFromXml("orginselect1");
-	String papername2 = exameName;
+	String papername2 = xmlData.getParamFromXml("papername2");
 	// 0-系统试卷；1专项考试；2-专项预习；3-专项课后；4-错题本；5推荐试卷
 	String orginselect2 = xmlData.getParamFromXml("orginselect2");
 	String newnamequiz = xmlData.getParamFromXml("newnamequiz");
 	@BeforeTest
 	public void BeforeSuite() {
-		System.setProperty("webdriver.chrome.driver", ChormeURL);
+		System.setProperty("webdriver.chrome.driver", "C:\\231\\chromedriver.exe");
 		this.driver = new ChromeDriver();
 		this.driver.manage().window().maximize();
 		AdminLoginPage adminLoginPage = new AdminLoginPage(this.driver, adminLoginUrl);
@@ -50,7 +49,7 @@ public class CouserPaperQuizTC {
 
 	// 添加试卷-课后题
 	@Test
-	public void ExercisesAfter() {
+	public void addcoursequiz() {
 		// 查询试卷
 		PaperListPage paperserach = new PaperListPage(this.driver, paperSearchUrl);
 		paperserach.Search(papername, orginselect);
@@ -79,16 +78,16 @@ public class CouserPaperQuizTC {
 			e.printStackTrace();
 		}
 		// 添加试题
-		CourseQuizAddPage addquiz = new CourseQuizAddPage(this.driver);
+		CourseQuizAdd addquiz = new CourseQuizAdd(this.driver);
 		addquiz.AddCourseQuiz();
 		paperserach.ElementExist();
 		// 删除功能检查
 		PaperListPage del = new PaperListPage(this.driver, cu2);
-		del.Delquiz();
+		del.delquiz();
 	}
 
 	@Test
-	public void PreparePaper() {
+	public void addcoursequiz1() {
 		// 查询试卷
 		PaperListPage paperserach = new PaperListPage(this.driver, paperSearchUrl);
 		paperserach.Search(papername1, orginselect1);
@@ -116,13 +115,13 @@ public class CouserPaperQuizTC {
 			e.printStackTrace();
 		}
 		// 添加试题
-		CourseQuizAddPage addquiz = new CourseQuizAddPage(this.driver);
+		CourseQuizAdd addquiz = new CourseQuizAdd(this.driver);
 		addquiz.AddCourseQuiz();
 		paperserach.ElementExist();
 	}
 
 	@Test
-	public void TeatPaper() {
+	public void addcoursequiz2() {
 		// 查询试卷
 		PaperListPage paperserach = new PaperListPage(this.driver, paperSearchUrl);
 		paperserach.Search(papername2, orginselect2);
@@ -150,7 +149,7 @@ public class CouserPaperQuizTC {
 			e.printStackTrace();
 		}
 		// 添加试题
-		CourseQuizAddPage addquiz = new CourseQuizAddPage(this.driver);
+		CourseQuizAdd addquiz = new CourseQuizAdd(this.driver);
 		addquiz.AddCourseQuiz();
 		paperserach.ElementExist();
 	}
